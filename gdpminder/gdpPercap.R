556e78bbb347717a5e7a5c4c0450c69530901fc4
jimmy shen <jimmy@takemobi.com>
2023-03-11 00:02:39 -0500

    Add gdpPercap vis

---
+++ gdpminder/gdpPercap.R
@@ -0,0 +1,17 @@
+list.of.packages <- c("ggplot2", "Rcpp")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library(ggplot2)
+library(plotly)
+library(gapminder)
+
+p <- gapminder %>%
+  filter(year==1977) %>%
+  ggplot( aes(gdpPercap, lifeExp, size = pop, color=continent)) +
+  geom_point() +
+  theme_bw()
+
+ggplotly(p)
+
+
